5240ac0a61a0be8d0525e892d9aba70d35440cb3
Peter Molnar <peter@kingmolnar.com>
2021-04-05 11:03:32 -0400

    update with summary

--- project/licensing/www/progress.php
+++ project/licensing/www/progress.php
@@ -77,7 +77,7 @@
 
 <HTML lang="eng">
     <HEAD>
-        <TITLE>Progressphp  </TITLE>
+        <TITLE>Progress</TITLE>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
